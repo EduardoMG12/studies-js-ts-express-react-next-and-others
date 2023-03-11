4774d8ceae6efec2a296791456ea0625b6cda633
Eduardo <eduardo@pop-os.localdomain>
2023-03-11 14:15:07 -0300

    others learning express

--- luizOtavioMiranda/express/hello-world/server.js
+++ luizOtavioMiranda/express/hello-world/server.js
@@ -3,13 +3,20 @@ import express from "express"
 
 const app = express()
 
+app.use(
+    express.urlencoded(
+        {
+            extended: true
+        }
+    )
+)
 
 
 app.get('/', (request, response) => {
     response.send(`
     <div style="display: flex; align-items: center; flex-direction: column; justify-content: center; background-color: gray; width: 100vw; height: 100vh; margin: 0; padding: 0; box-sizing:border-box">
     <form action="/" method="POST" >
-        <label style="font-size:32px; color: white">Name:</label><input type="text" name="name"/>
+        <label style="font-size:32px; color: white">Name:</label><input type="text" name="userName"/>
         <button>Submit</button>
     </form>
     <p style="font-size:32px; color: white">hello <b>world</b></p>
@@ -17,10 +24,27 @@ app.get('/', (request, response) => {
     `)
 })
 
+app.get('/home/:userId?', (request, response) => {
+    request.params.userId ?
+        response.send(
+            ` <div style="display: flex; align-items: center; flex-direction: column; justify-content: center; background-color: gray; width: 100vw; height: 100vh; margin: 0; padding: 0; box-sizing:border-box">
+            <p style="font-size:32px; color: white">I am to the home with params id: ${request.params.userId}</p>
+            </div>`
+        )
+        :
+        response.send(`
+    <div style="display: flex; align-items: center; flex-direction: column; justify-content: center; background-color: gray; width: 100vw; height: 100vh; margin: 0; padding: 0; box-sizing:border-box">
+    
+    <p style="font-size:32px; color: white">I am to the home</b></p>
+    </div>
+    `)
+})
+
+
 app.post("/", (request, response) => {
     response.send(`
     <div style="display: flex; align-items: center; flex-direction: column; justify-content: center; background-color: gray; width: 100vw; height: 100vh; margin: 0; padding: 0; box-sizing:border-box">
-        <p style="font-size:32px; color: white">Recebi o <b>formulario</b></p>
+        <p style="font-size:32px; color: white">Recive form: <b>${request.body.userName}</b></p>
     </div>`)
 })
 
