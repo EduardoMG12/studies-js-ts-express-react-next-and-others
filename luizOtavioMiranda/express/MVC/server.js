12f3da26783ed7030ba51038ed6fbe002272ec6e
Eduardo <eduardo@pop-os.localdomain>
2023-03-23 15:14:23 -0300

    learning configs express and how created project beggin structure, depencies and somethings

--- luizOtavioMiranda/express/MVC/server.js
+++ luizOtavioMiranda/express/MVC/server.js
@@ -6,6 +6,7 @@ global.__dirname = path.resolve('.')
 const app = express()
 
 app.use(express.urlencoded({ extended: true }))
+app.use(express.static(path.resolve(__dirname, 'public'))
 app.set('views', path.resolve(__dirname, 'src', 'views'))
 app.set('view engine', 'ejs')
 
