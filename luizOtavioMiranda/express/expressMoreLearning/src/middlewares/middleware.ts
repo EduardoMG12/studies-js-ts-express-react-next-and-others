72c510259b195ed1ac2d4e5bf8b7b450966e25a1
Eduardo <eduardo@pop-os.localdomain>
2023-03-24 18:05:41 -0300

    feat(middlewares): added middlewares for csurf

---
+++ luizOtavioMiranda/express/expressMoreLearning/src/middlewares/middleware.ts
@@ -0,0 +1,8 @@
+export const checkCsurfError = ({ err, req, res, next }: any) => {
+    if (err && err.code === "EBADCSRFTOKEN") res.send("BAD CSURF")
+}
+
+export const csurfMiddlewareToken = ({ req, res, next }: any) => {
+    res.locals.csurfToken = req.csurfToken();
+    next()
+}
